5a74016112c014feea1cf1930a1d80992e2b6799
tonyfilho <oficinadosdrones@gmail.com>
2024-06-24 16:29:35 +0100

    style header.component

--- src/app/body/header/header.component.ts
+++ src/app/body/header/header.component.ts
@@ -1,12 +1,22 @@
+import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  localLogin$!: Observable<boolean>;
+  localImg$!: Observable<string>;
+
+
+  constructor () {
+    this.localLogin$ = new Observable(d => d.next(false))
+    this.localImg$ = new Observable((d: any ) => d == null ? d.next("") : d.next("./../../../assets/images/login/no_avatar.png"))
+  }
 
 }
