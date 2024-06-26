12c7c353122ddb7fecd1c3e589d627df0bd5d0c1
tonyfilho <oficinadosdrones@gmail.com>
2024-06-26 13:38:25 +0100

    update angular 18 done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 
+
 import { Routes } from '@angular/router';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
@@ -7,6 +8,6 @@ export const routes: Routes = [
   { path: '', redirectTo: 'section', pathMatch: 'full' },
   {path:"about", loadComponent: ()  => import('./pages/about/about.component').then(m => m.AboutComponent)},
   {path:"not-found", component: NotFoundComponent},
-  // { path: '**', redirectTo: () => { return '/not-found'} , pathMatch: 'full' }
+  { path: '**', redirectTo: () => { return '/not-found'} , pathMatch: 'full' }
 
 ];
