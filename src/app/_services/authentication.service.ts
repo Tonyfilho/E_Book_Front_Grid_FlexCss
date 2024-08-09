f8fe061c460ef5c9d38c6d07cb28bb3de5d55d94
tonyfilho <oficinadosdrones@gmail.com>
2024-08-09 09:11:10 +0100

    update diretories

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -1,22 +1,19 @@
-import { Auth, getAuth, GoogleAuthProvider, signOut, UserCredential } from '@angular/fire/auth';
+import { Auth, getAuth, GoogleAuthProvider, UserCredential } from '@angular/fire/auth';
 
 
 
 import { Injectable } from '@angular/core';
 
 
-import { BehaviorSubject, catchError, from, Observable, tap, throwError } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 
 
-import { HttpErrorResponse } from '@angular/common/http';
 import { FirebaseApp } from '@angular/fire/app';
 
-import { signInWithEmailAndPassword } from 'firebase/auth';
 
 
 import { Location } from '@angular/common';
 import { IGoogleToken } from '../_models/interface/google-token';
-import { ModelTokenResponse } from '../_models/model/model-google-signin';
 import { SnackBarService } from '../_share/snack-bar/snack-bar.service';
 import { UnSubscription } from '../_share/UnSubscription';
 
@@ -50,114 +47,9 @@ export class AuthenticationService extends UnSubscription {
   ) {
     super();
     this.auth = getAuth(firebaseApp);
-    this.autoLogin();
+   
   }
 
-  private handAuthentication = () => {
-    this.tokenResponse$.pipe(tap((localUserToken: IGoogleToken | null) => {
-      return localStorage.setItem('userToken', JSON.stringify(localUserToken)); //Quardaremos em LocalStorage um String com todos os Dados transformado em Json.
-
-    })).subscribe();
-
-  }
-
-  signIn(params: SingIn): Observable<UserCredential> {
-    return from(signInWithEmailAndPassword(this.auth, params.email, params.password)).pipe(tap((res: UserCredential | any) => {
-      const expirationDate = new Date(new Date().getTime() + +res?._tokenResponse['expiresIn'] * 1000);
-      const localUserToken = new ModelTokenResponse(res?.user.email, res?._tokenResponse.kind,
-        res._tokenResponse?.['localId'], res?.user.displayName, res?.user.accessToken, res._tokenRespons?.refreshToken, expirationDate,
-        res.user?.['photoURL'] == null ? './../../../../assets/images/login/no_avatar.png' : res.user?.['photoURL']);
-      this.tokenResponse$.next(localUserToken); //Esta variavel é para salvar o token
-      this.userCredential$.next(res && res.user);
-      this.isLoginAuthorization$.next(true);
-    }))
-      .pipe(catchError((e: HttpErrorResponse) => {
-        this.snackService.openSnackBar(5000, e.message);
-        console.log("error no server: ", e)
-        return throwError(() => e.message);
-      }));
-  }
-
-  /**Podemo usar a nova sintax do JS */
-
-  signInUserCredential = (params: SingIn) => {
-    this.handAuthentication();
-    const localRespose = signInWithEmailAndPassword(this.auth, params.email, params.password);
-    return from(localRespose).pipe(tap((res: UserCredential | any) => {
-      const expirationDate = new Date(new Date().getTime() + +res?._tokenResponse['expiresIn'] * 1000);
-      const localUserToken = new ModelTokenResponse(res?.user.email, res?._tokenResponse.kind,
-        res._tokenResponse?.['localId'], res?.user.displayName, res?.user.accessToken, res._tokenRespons?.refreshToken, expirationDate,
-        res.user?.['photoURL'] == null ? './../../../../assets/images/login/no_avatar.png' : res.user?.['photoURL']);
-      this.tokenResponse$.next(localUserToken); //Esta variavel é para salvar o token
-      this.userCredential$?.next(res && res.user);
-      this.isLoginAuthorization$.next(true);
-    })).pipe(catchError((e: HttpErrorResponse) => {
-      //    console.log("Error do catchError: ", e);
-      this.snackService.openSnackBar(5000, e.message);
-      return throwError(() => e.message);
-    }));
-  }
-
-  /**
-   * autoLogin will put in the AppComponent and everytime it will be reload
-   * the data will load from LOCALSTORAGE.
-   * 1º I need to check if already  signIn if doenst have , the Observable VAR hasUserLogin will be null
-   * 2º If got a token I update the Observable User VAR
-   * 3ª create a expiration date and calc the time after signIn
-   * 4º start autoLogout
-   * 5º The method must be called in the OnInit() and never in the Constructor.
-   */
-
-    autoLogin = () => {
-     const localGetStorage: IGoogleToken = JSON.parse(localStorage.getItem('userToken') as string);
-    // console.log("LocalStorage ", localGetStorage);
-    if (!localGetStorage) {
-      return;
-    }
-    const localTokenResponse: IGoogleToken  =  new ModelTokenResponse(localGetStorage?.email, localGetStorage?.kind,
-      localGetStorage?.['localId'], localGetStorage.displayName, localGetStorage.idToken, localGetStorage?.refreshToken, localGetStorage.expiresIn,
-      localGetStorage['avatar'] == null ? './../../../../assets/images/login/no_avatar.png' : localGetStorage?.['avatar']);
-      this.tokenResponse$.next(localTokenResponse);
-      let localExpirationTime = new Date(localGetStorage.expiresIn).getTime() - new Date().getTime();
-     this.autoLogOut(localExpirationTime);
-
-
-
-    }
-
-
-
-
-  /**
-   *
-   * @param expirationDate Get time of the Firebase Auth API and calc the expieration date
-   * after the expieration data done, it will call the logout() method.
-   */
-  private autoLogOut = (expirationDate: number) =>  {
-    this.tokenExpirationTimer = setTimeout (() => {
-      this.logOut();
-    }, expirationDate);
-
-  }
-
-
-
-  /**
-  * this method will called by user in Logout button and in
-  * authoLogout() method.
-  */
-  logOut = () => {
-    signOut(this.auth);
-    this.tokenResponse$.next(null);  //limpando Observable
-    localStorage.removeItem("userToken"); //limpando o LocalStore
-    this.isLoginAuthorization$.next(false);
-    this.location.go('/about');
-    if (this.tokenExpirationTimer) {
-      clearTimeout(this.tokenExpirationTimer);
-    }
-    this.tokenExpirationTimer = null;
-
-  }
 
 
 }
