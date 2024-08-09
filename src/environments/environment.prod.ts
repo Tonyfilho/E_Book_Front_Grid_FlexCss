e962c9e455a6cd6e5f4d9f4eddbd1c9ea440a106
tonyfilho <oficinadosdrones@gmail.com>
2024-08-09 13:34:25 +0100

    provider

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyClgf5wXKFXTjd_WVGfRIFgMvTXHM6yZlE",
+    authDomain: "ebook-front-grid-flex.firebaseapp.com",
+    projectId: "ebook-front-grid-flex",
+    storageBucket: "ebook-front-grid-flex.appspot.com",
+    messagingSenderId: "8851151446",
+    appId: "1:8851151446:web:a3cb2bbb06250767db79f7"
+  },
+
+  production: true
+
+};
+
+
---
+++ src/environments/environment.ts
@@ -0,0 +1,19 @@
+
+
+
+// Your web app's Firebase configuration
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyClgf5wXKFXTjd_WVGfRIFgMvTXHM6yZlE",
+    authDomain: "ebook-front-grid-flex.firebaseapp.com",
+    projectId: "ebook-front-grid-flex",
+    storageBucket: "ebook-front-grid-flex.appspot.com",
+    messagingSenderId: "8851151446",
+    appId: "1:8851151446:web:a3cb2bbb06250767db79f7"
+  },
+
+  production: false
+
+
+};
+
